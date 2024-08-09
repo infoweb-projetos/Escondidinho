cb90de3543cd903fa406ae4139ed1711740b4311
beckyxz <rebecaojuare10@gmail.com>
2024-08-09 11:43:09 -0300

    progresso 40%

--- escondidinho/src/pages/Login.js
+++ escondidinho/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assets/css/styles.css';
-import logo from '../assets/img/logo_teste.png';
+import logo from '../assets/img/logo 1.png';
 
 const Login = () => {
   return (
@@ -8,18 +8,24 @@ const Login = () => {
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-      <h1>Login</h1>
+     {/*  <h1>Login</h1> */}
       <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <label htmlFor="email"></label>
+        <input type="email" id="email" name="email" required  placeholder='email'/>
+        <label htmlFor="password"></label>
+        <input type="password" id="password" name="password" required placeholder='senha'/>
         
-        <label htmlFor="password">Senha</label>
-        <input type="password" id="password" name="password" required />
-        
+        <div className="remember-forgot"> 
+          <label>  <input  type="checkbox"/> Lembre de mim </label>
+          <a href="#"> Esqueceu a senha?</a>
+        </div>
         <button type="submit">Entrar</button>
       </form>
+      
+      
       <p>Não tem uma conta? <a href="/register">Cadastre-se</a></p>
     </div>
+    
   );
 };
 
