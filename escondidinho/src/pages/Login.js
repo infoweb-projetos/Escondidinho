cb32cc048fd1d7bfb1278503a360dda20fe6f268
artur-vinicius <artur.t@escolar.ifrn.edu.br>
2024-08-09 19:28:09 -0300

    teste - login

--- escondidinho/src/pages/Login.js
+++ escondidinho/src/pages/Login.js
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../assets/css/styles.css';
 import logo from '../assets/img/logo 1.png';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/auth/login', {
+        email,
+        password,
+      });
+
+      setMessage('Login realizado com sucesso!');
+      // Redirecionar ou salvar token JWT se necessário
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setMessage('E-mail ou senha incorretos.');
+      } else {
+        setMessage('Erro ao tentar realizar o login.');
+      }
+    }
+  };
+
   return (
     <div className="container">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-     {/*  <h1>Login</h1> */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email"></label>
-        <input type="email" id="email" name="email" required  placeholder='email'/>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="email"
+        />
         <label htmlFor="password"></label>
-        <input type="password" id="password" name="password" required placeholder='senha'/>
-        
-        <div className="remember-forgot"> 
-          <label> <input  type="checkbox"/> Lembre de mim </label>
-          <a class="forgot" href="#"> Esqueceu a senha?</a>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          placeholder="senha"
+        />
+
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Lembre de mim
+          </label>
+          <a className="forgot" href="#">
+            Esqueceu a senha?
+          </a>
         </div>
         <button type="submit">Entrar</button>
       </form>
-      
-      
-      <p>Não tem uma conta? <a href="/register">Cadastre-se</a></p>
+
+      {message && <p>{message}</p>}
+
+      <p>
+        Não tem uma conta? <a href="/register">Cadastre-se</a>
+      </p>
     </div>
-    
   );
 };
 
