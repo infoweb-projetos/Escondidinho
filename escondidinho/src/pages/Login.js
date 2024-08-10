dc06856d87ef1988a3e83139e98a9124a33fc6fb
Yuri Mateus Lira da Costa <y.costa@academico.ifrn.edu.br>
2024-08-10 12:00:43 -0300

    implentação do método post

--- escondidinho/src/pages/Login.js
+++ escondidinho/src/pages/Login.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../assets/css/styles.css';
 import logo from '../assets/img/logo 1.png';
 
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        email,
+        password,
+      });
+      alert('Login realizado com êxito');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      alert('Erro ao fazer login');
+    }
+  };
+
   return (
     <div className="container">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-     {/*  <h1>Login</h1> */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email"></label>
-        <input type="email" id="email" name="email" required  placeholder='email'/>
+        <input
+          type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email"/>
         <label htmlFor="password"></label>
-        <input type="password" id="password" name="password" required placeholder='senha'/>
-        
-        <div className="remember-forgot"> 
-          <label> <input  type="checkbox"/> Lembre de mim </label>
-          <a class="forgot" href="#"> Esqueceu a senha?</a>
+        <input
+          type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Senha"/>
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Lembre de mim
+          </label>
+          <a className="forgot" href="#">Esqueceu a senha?</a>
         </div>
         <button type="submit">Entrar</button>
       </form>
-      
-      
       <p>Não tem uma conta? <a href="/register">Cadastre-se</a></p>
     </div>
-    
   );
 };
+
 
 export default Login;
--- escondidinho/src/pages/Register.js
+++ escondidinho/src/pages/Register.js
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../assets/css/styles.css';
 import logo from '../assets/img/logo 1.png';
 
+
 const Register = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [tel, setTel] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+   
+    if (password !== confirmPassword) {
+      alert('As senhas não são as mesmas!');
+      return;
+    }
+
+
+    try {
+      const response = await axios.post('http://localhost:5000/register', {
+        nome,
+        email,
+        tel,
+        password,
+      });
+      alert('usuário registrado');
+    } catch (error) {
+      console.error('erro ao registrar o usuário:', error);
+      alert('erro ao registrar o usuário');
+    }
+  };
+
+
   return (
     <div className="container">
       <div className="logo">
         <img src={logo} alt="Logo" />
       </div>
-     {/*  <h1>Login</h1> */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="nome"></label>
-        <input type="nome" id="nome" name="nome" required  placeholder='nome'/>
-        <label htmlFor="email"></label>
-        <input type="email" id="email" name="email" required  placeholder='email'/>
-        <input type="tel" id="tel" name="tel" required  placeholder='tel/cel'/>
-        <input type="password" id="password" name="password" required placeholder='senha'/>
-        <input type="password" id="password" name="password" required placeholder='Confirmar senha'/>
-       
+        <input
+          type="text" id="nome" name="nome" value={nome} onChange={(e) => setNome(e.target.value)} required placeholder="Nome"/>
+               <label htmlFor="email"></label>
+        <input
+          type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="Email"/>
+        <input
+          type="tel" id="tel" name="tel" value={tel} onChange={(e) => setTel(e.target.value)} required placeholder="Telefone"/>
+        <input
+          type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="Senha"/>        
+        <input
+          type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required placeholder="Confirmar senha"/>        
         <button type="submit">Registrar</button>
       </form>
-      
-      
-
     </div>
   );
 };
+
 
 export default Register;
