5dda0ca61f0a8fdb3ce11efdcff40c4805260c49
YuriMateuss <y.mateus@escolar.ifrn.edu.br>
2025-01-16 16:33:54 -0300

    Página de mudar senha ajustada

--- escondidinho/src/pages/NovaSenha.js
+++ escondidinho/src/pages/NovaSenha.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { auth } from '../firebase'; // Importando o auth do Firebase
+import { auth } from '../firebase';
 import { confirmPasswordReset } from 'firebase/auth';
+import '../assets/css/NovaSenha.css';
 
 const NovaSenha = () => {
   const location = useLocation();
@@ -12,7 +13,7 @@ const NovaSenha = () => {
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    setOobCode(queryParams.get('oobCode')); // Captura o oobCode da URL
+    setOobCode(queryParams.get('oobCode'));
   }, [location]);
 
   const handleResetPassword = async (e) => {
@@ -23,8 +24,7 @@ const NovaSenha = () => {
     }
 
     try {
-      // Chama a função do Firebase para redefinir a senha
-      await confirmPasswordReset(auth, oobCode, newPassword); // Usando o oobCode e a nova senha
+      await confirmPasswordReset(auth, oobCode, newPassword);
       setMessage('Senha redefinida com sucesso!');
     } catch (error) {
       setMessage(`Erro ao redefinir senha: ${error.message}`);
@@ -32,26 +32,30 @@ const NovaSenha = () => {
   };
 
   return (
-    <div>
-      <h2>Redefinir Senha</h2>
-      <form onSubmit={handleResetPassword}>
-        <input
-          type="password"
-          placeholder="Nova senha"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirmar nova senha"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Redefinir Senha</button>
-        {message && <p>{message}</p>}
-      </form>
+    <div className="nova-senha-container">
+      <div className="form-container">
+        <h2>Redefinir Senha</h2>
+        <form onSubmit={handleResetPassword} className="reset-form">
+          <input
+            type="password"
+            placeholder="Nova senha"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            required
+            className="input-field"
+          />
+          <input
+            type="password"
+            placeholder="Confirmar nova senha"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            className="input-field"
+          />
+          <button type="submit" className="submit-button">Redefinir Senha</button>
+        </form>
+        {message && <p className="message">{message}</p>}
+      </div>
     </div>
   );
 };
