5a3d8b0f03b47b696d7186c52b1d2bdf6ef83cb1
beckyxz <rebecaojuare10@gmail.com>
2025-01-16 16:05:54 -0300

    chooserole ok

--- escondidinho/src/pages/ChooseRole.js
+++ escondidinho/src/pages/ChooseRole.js
@@ -2,21 +2,38 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import RoundedButton from './RoundedButton';
 import styles from '../assets/css/chooserole.module.css';
+import logo from '../assets/img/logo.png';
+import eucompro from '../assets/img/eucompro.svg';
+import euvendo from '../assets/img/euvendo.svg';
 
 const ChooseRole = () => {
   const navigate = useNavigate();
 
   return (
+
     <div className={styles.container}>
+     
       <div className={styles.content}>
-        <h2>Você é...?</h2>
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo" />
+      </div>
+        <h2>O que você faz?</h2>
+        <div className={styles.blocos}>
+        <div className={styles.eucompro}>
+        <img src={eucompro} alt="Logo" />
+      </div>
+      <div className={styles.euvendo}>
+        <img src={euvendo} alt="Logo" />
+      </div>
+      </div>
         <div className={styles.roleButtons}>
+        
           <RoundedButton 
-            text="Cliente" 
+            text="Eu compro!" 
             onClick={() => navigate('/register/cliente')} 
           />
           <RoundedButton 
-            text="Vendedor" 
+            text="Eu vendo!" 
             onClick={() => navigate('/register/vendedor')} 
           />
         </div>
